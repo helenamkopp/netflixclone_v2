0245b898338a25f12de7351bdbf846ab1fc8121c
Helena Kopp <helena.mkopp@gmail.com>
2020-09-23 13:20:34 -0300

    -

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect }from 'react';
+import './App.css'
+import Tmdb from './Tmdb';
+import Header from './components/Header'
+import FeaturedMovie from './components/FeaturedMovie'
+import MovieRow from './components/MovieRow'
+import Footer from './components/Footer'
 
-function App() {
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o featured - filme em destaque
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+
+    }
+
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          < MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      < Footer />
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media1.giphy.com/media/cnzP4cmBsiOrccg20V/giphy.gif?cid=ecf05e47gluxe8lax5jewiyiy2nfu1a9a2vtcsinrhbcbves&rid=giphy.gif" alt="Loading" />
+        </div>
+      }
+
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+export default () => {
+  return (
+    <footer>
+      Desenvolvido por Helena Meyer em 2020 <br/>
+      Aprendizados em React <br/>    
+    </footer>
+  )
+}
+
